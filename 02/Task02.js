c4db2bebe636682df217ed8b75b9f6caa93347c9
Diana Wiaduch <diana.wiaduch@onet.pl>
2024-10-28 21:56:46 +0100

    add task 02

--- 02/Task02.js
+++ 02/Task02.js
@@ -1,15 +1,25 @@
-import React from 'react';
-import Welcome from './../src/components/Welcome';
-import withPopup from './../src/hoc/withPopup';
-
-const ComponentWithPopup = withPopup(Welcome);
+import React, { useState } from "react";
+import Welcome from "./../src/components/Welcome";
+import withPopup from "./../src/hoc/withPopup";
+const styles = {
+  position: "absolute",
+  left: "50%",
+  top: "50%",
+  transform: "translate(-50%, -50%)",
+  height: "100vh",
+  width: "100%",
+  backdropFilter: "blur(2px)",
+  display: "flex",
+  display: "grid",
+  placeContent: "center",
+};
+const ComponentWithPopup = withPopup(styles)(Welcome);
 
 const Task02 = () => (
-    <section>
-        <h2>Task 02</h2>
-        <ComponentWithPopup />
-    </section>
-)
+  <section>
+    <h2>Task 02</h2>
+    <ComponentWithPopup />
+  </section>
+);
 
 export default Task02;
-
--- src/hoc/withPopup.js
+++ src/hoc/withPopup.js
@@ -1,8 +1,31 @@
 import React from 'react';
 
-const withPopup = () => {
-    return () => <section>withPopup()</section>;
-}
+const withPopup = (styles) => (WrappedComponent) => {
+  return class extends React.Component {
+    state = {
+      isOpened: true,
+    };
+
+    closePopup = (e) => {
+      e.preventDefault();
+      this.setState({ isOpened: false });
+    };
+    render() {
+      const { isOpened } = this.state;
+
+      if (isOpened) {
+        return (
+          <section style={styles}>
+            <WrappedComponent />
+            <button onClick={(e) => this.closePopup(e)}>close</button>
+          </section>
+        );
+      }
+
+      return <WrappedComponent />;
+    }
+  };
+}; 
 
 
 export default withPopup;
